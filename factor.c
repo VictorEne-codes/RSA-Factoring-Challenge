521ce721d586ed04c4b09086ea945bce8b6e7b49
VictorEne-Codes <omalevicene@gmail.com>
2024-10-06 12:48:58 -0700

    trying again

--- factor.c
+++ factor.c
@@ -1,60 +1,23 @@
-#include "factor.h"
+#include <stdio.h>
 
-/**
- * main - enty to main function
- * Return: void
- */
-
-int main(int argc, char *argv[])
+int main()
 {
-	FILE *ptr;
-	size_t counter;
-	ssize_t line_number;
-	char *buff = NULL;
+    long long int n = 239809320265259;
+    long int f1 = 2;
+    long int f2;
 
+    while (n % f1)
+    {
+        if (f1 <= n)
+        {
+            f1++;
+        }
+        else {
+            return (-1);
+        }
+    }
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "Usage: factor <file>\n");
-		exit(EXIT_FAILURE);
-	}
-	ptr = fopen(argv[1], "r");
-	if (ptr == NULL)
-	{
-		fprintf(stderr, "Error: can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while((line_number = getline(&buff, &counter, ptr)) != -1)
-	{
-		factorize(buff);
-	}
-return (0);
-}
-
-
-/**
- * factorize - The function factorize a number
- * @buffer: pointer to the address of the number
- *
- * Return: int
- */
-int factorize(char *buffer)
-{
-
-	u_int32_t num;
-	u_int32_t i;
-
-	num = atoi(buffer);
-
-
-	for (i = 2; i < num; i++)
-	{
-		if (num % i == 0)
-		{
-			printf("%d=%d*%d\n",num,num/i,i);
-			break;
-		}
-	}
-
-return (0);
+    f2 = n / f1;
+    printf("%lld = %ld * %ld\n", n, f2, f1);
+    return (0);
 }
